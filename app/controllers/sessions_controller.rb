e170331f9173a62eba42de9b09a1a85cc54c373d
alecorleonis <alejandramleon@unitec.edu>
2015-06-05 15:51:34 -0600

    cambios de :notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ def create
   user = User.authenticate(params[:email], params[:password])
   if user
     session[:user_id] = user.id
-    redirect_to "/requests", :notice => "Conectado!"
+    redirect_to "/requests"
   else
     redirect_to root_url, :notice => "Correo o contraseña incorrecto!"
     
